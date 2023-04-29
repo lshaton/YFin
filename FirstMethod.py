c4e3701958fc7452e7b09f987cbd0f4a1e5ff3dd
Leonid SHaton <lshaton@msn.com>
2023-04-29 21:00:07 +0300

    MSFT Commit

--- FirstMethod.py
+++ FirstMethod.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import yfinance as yf
 from yahoofinancials import YahooFinancials
-aapl_df = yf.download('GOOG', 
+aapl_df = yf.download('MSFT', 
                       start='2019-01-01', 
                       end='2021-06-12', 
                       progress=False,
